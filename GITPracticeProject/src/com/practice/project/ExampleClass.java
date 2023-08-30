e8dae46e70a515eebc7f304dd4836122cb922327
AshwiniBais1991 <ashwini.bais1991@gmail.com>
2023-08-30 19:29:17 +0530

    substration code is added

---
+++ GITPracticeProject/src/com/practice/project/ExampleClass.java
@@ -0,0 +1,9 @@
+package com.practice.project;
+
+public class ExampleClass {
+	public static void getSubstraction(int num1, int num2) {
+		int result = num1-num2;
+		System.out.println("sustraction of number is>>>" + result);
+	}
+
+}
--- GITPracticeProject/src/com/practice/project/MainClass.java
+++ GITPracticeProject/src/com/practice/project/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		ExampleGIT1 exampleGIT1 = new ExampleGIT1();
 		exampleGIT1.getAdditionOfNumber(20, 50);
+		ExampleClass.getSubstraction(50, 20);
 	} 
 
 }
